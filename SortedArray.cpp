056c508967892949b68ddcbbce915203aebd0439
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2021-11-15 13:00:03 -0300

    Function List + Menu + main
    Made a correction in the Read function, which was always returning 1, but it was meant to return the position of the element found. Increased sleep time after list function to improve user capacity to read all listed data.

--- SortedArray.cpp
+++ SortedArray.cpp
@@ -43,7 +43,7 @@ int Read(int Key){
     for (; Lista[i].Id < Key; i++){
       continue;
     }
-    if (i != n+1 and Lista[i].Id == Key) return 1;
+    if (i != n+1 and Lista[i].Id == Key) return i;
   }
   return 0;
 }
@@ -91,3 +91,76 @@ OpConclusions Delete(int Key){
   }
   else return ListaVazia;
 }
+
+OpConclusions Listagem(){
+  if (n>0){
+    for (int i=1; i<=n; i++){
+      cout << "ID do Registro: " << Lista[i].Id << "\nInfo: " << Lista[i].Info << endl;
+    }
+    return Success;
+  }
+  else return ListaVazia;
+  sleep(n*3); 
+}
+
+bool Menu(){
+  enum opcoes {Buscar = 1, Inserir, Excluir, Atualizar, Listar, Abandonar};
+  int opCrud;
+  int chaveUsuario, InfoUsuario; 
+  string MSG = "";
+  const string MENU_OPTIONS = "Listas Lineares Não Ordenadas\n\nEscolha sua opção: \n1.Buscar\n2.Inserir\n3.Excluir\n4.Atualizar\n5.Listar Registros\n6.Deixar programa\n\nDigite aqui: ";
+  cout << MENU_OPTIONS;
+  cin >> opCrud;
+  switch (opCrud){
+    case Buscar:
+      cout << headerline << endl << "MENU BUSCAR\nQual o número da chave do registro? " ; 
+      cin >> chaveUsuario;
+      if (Read(chaveUsuario) != NotFound){
+        cout << "ID do Registro: " << chaveUsuario << "\nInfo: " << Lista[Read(chaveUsuario)].Info << endl;
+      }
+      else cout << ReportConclusion(NotFound) << endl;
+      break;
+    case Inserir:
+      cout << headerline << endl << "MENU INSERIR\nCrie uma chave para o novo dado: ";
+      cin >> chaveUsuario;
+      cout << "Qual o dado a ser inserido nessa chave? ";
+      cin >> InfoUsuario;
+      cout << ReportConclusion(Create(chaveUsuario, InfoUsuario)) << endl;
+      break;
+    case Excluir:
+      cout << headerline << endl << "MENU EXCLUIR\nQual a chave do dado que deve ser excluído? ";
+      cin >> chaveUsuario;
+      cout << ReportConclusion(Delete(chaveUsuario)) << endl;
+      break;
+    case Atualizar:
+      cout << headerline << endl << "MENU ATUALIZAR\nQual a chave do dado a ser atualizado? ";
+      cin >> chaveUsuario;
+      cout << "Qual o novo valor a ser inserido? ";
+      cin >> InfoUsuario;
+      cout << ReportConclusion(Update(chaveUsuario, InfoUsuario)) << endl;
+      break;
+    case Listar:
+      cout << headerline << endl << "MENU LISTAR\n" << endl;
+      cout << ReportConclusion(Listagem()) << endl;
+      break;
+    case Abandonar:
+      system("clear");
+      cout << "Fim de execução." << endl;
+      return true;
+    default:
+      system("clear");
+      cout << "Operação não reconhecida. Tente novamente.\n" << endl;
+  }
+  sleep(2);
+  system("clear");
+  return false;
+}
+
+int main() {
+  bool finishProgram = false;
+  cout << "Bem-vindo ao algoritmo de Listas Lineares Sequenciais Ordenadas com chave identificadora.\n " << endl;
+  do{
+    finishProgram = Menu();
+  }
+  while (!finishProgram);
+} 
